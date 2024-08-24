0c7b2e0894d321b072f3edb7081eaf87407e68fc
Gwins7 <Gwins777@qq.com>
2024-08-25 01:57:33 +0800

    n-ext: update n-ext-test design

--- apps/dasics-test/test/n-ext-test.c
+++ apps/dasics-test/test/n-ext-test.c
@@ -7,40 +7,29 @@
 
 #include "udasics.h"
 
-#define CAUSE_IRQ_U_EXT ((uint64_t)((1ULL<<63) | 8))
 
 const char *test_info = "[MAIN] N-extension u-timer interrupt test\n";
 
-extern void u_int_entry(void);
-
 void exit_function(void) {
 	printf("[MAIN] u-timer interrupt test finished\n");
-}
-
-void u_int_handler(void) {
-	uint64_t ucause = csr_read(ucause);
-    uint64_t utval = csr_read(utval);
-    uint64_t uepc = csr_read(uepc);
-	printf("[U_INT_HANDLER] catch u-int, ucause = 0x%lx, uepc = 0x%lx, utval = 0x%lx\n", ucause, uepc, utval);
-	if (ucause == CAUSE_IRQ_U_EXT){
-		printf("[U_INT_HANDLER] clear timer\n");
-		csr_write(0x045,0);
-	}
 }
 
 int main(void) {
 	atexit(exit_function);
 
 	printf(test_info);
-	
+
+    prepare_u_int();
 	csr_write(0x045,0);
 
 	for (int i=0;i<10;i++){
 		printf("[U_INT_HANDLER] set timer for %d time(s).\n",i);
-		csr_write(0x045, 50*1000000); // 1s
-		while (csr_read(0x045) != 0);
+		csr_write(0x045, 1000000);
+		uint64_t count;
+		while ((count = csr_read(0x045)) != 0);
 	}
 
+	clear_u_int();
 	exit(0);
 	return(0);
 }
