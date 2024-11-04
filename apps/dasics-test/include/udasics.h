0c35d6e7d5038cc8aebf0c064a52a700fb2c1f8d
Gwins7 <gwins777@qq.com>
2024-11-04 15:02:08 +0800

    fdi-nh5-new: init

--- apps/dasics-test/include/udasics.h
+++ apps/dasics-test/include/udasics.h
@@ -10,16 +10,11 @@
 
 /* Add dasics exceptions */
 #define EXC_DASICS_UFETCH_FAULT     24
-#define EXC_DASICS_SFETCH_FAULT     25
-#define EXC_DASICS_ULOAD_FAULT      26
-#define EXC_DASICS_SLOAD_FAULT      27
-#define EXC_DASICS_USTORE_FAULT     28
-#define EXC_DASICS_SSTORE_FAULT     29
-#define EXC_DASICS_UECALL_FAULT     30
-#define EXC_DASICS_SECALL_FAULT     31
-
+#define EXC_DASICS_ULOAD_FAULT      25
+#define EXC_DASICS_USTORE_FAULT     26
+#define EXC_DASICS_UECALL_FAULT     27
 /* DASICS csrs */
-#define CSR_DUMCFG          0x9e0
+#define CSR_DUMCFG          0x9e1
 #define CSR_DUMBOUNDLO      0x9e2
 #define CSR_DUMBOUNDHI      0x9e3
 
@@ -40,30 +35,30 @@
 #define CSR_DLBOUND2HI      0x895
 #define CSR_DLBOUND3LO      0x896
 #define CSR_DLBOUND3HI      0x897
-// #define CSR_DLBOUND4LO      0x898
-// #define CSR_DLBOUND4HI      0x899
-// #define CSR_DLBOUND5LO      0x89a
-// #define CSR_DLBOUND5HI      0x89b
-// #define CSR_DLBOUND6LO      0x89c
-// #define CSR_DLBOUND6HI      0x89d
-// #define CSR_DLBOUND7LO      0x89e
-// #define CSR_DLBOUND7HI      0x89f
-// #define CSR_DLBOUND8LO      0x8a0
-// #define CSR_DLBOUND8HI      0x8a1
-// #define CSR_DLBOUND9LO      0x8a2
-// #define CSR_DLBOUND9HI      0x8a3
-// #define CSR_DLBOUND10LO     0x8a4
-// #define CSR_DLBOUND10HI     0x8a5
-// #define CSR_DLBOUND11LO     0x8a6
-// #define CSR_DLBOUND11HI     0x8a7
-// #define CSR_DLBOUND12LO     0x8a8
-// #define CSR_DLBOUND12HI     0x8a9
-// #define CSR_DLBOUND13LO     0x8aa
-// #define CSR_DLBOUND13HI     0x8ab
-// #define CSR_DLBOUND14LO     0x8ac
-// #define CSR_DLBOUND14HI     0x8ad
-// #define CSR_DLBOUND15LO     0x8ae
-// #define CSR_DLBOUND15HI     0x8af
+#define CSR_DLBOUND4LO      0x898
+#define CSR_DLBOUND4HI      0x899
+#define CSR_DLBOUND5LO      0x89a
+#define CSR_DLBOUND5HI      0x89b
+#define CSR_DLBOUND6LO      0x89c
+#define CSR_DLBOUND6HI      0x89d
+#define CSR_DLBOUND7LO      0x89e
+#define CSR_DLBOUND7HI      0x89f
+#define CSR_DLBOUND8LO      0x8a0
+#define CSR_DLBOUND8HI      0x8a1
+#define CSR_DLBOUND9LO      0x8a2
+#define CSR_DLBOUND9HI      0x8a3
+#define CSR_DLBOUND10LO     0x8a4
+#define CSR_DLBOUND10HI     0x8a5
+#define CSR_DLBOUND11LO     0x8a6
+#define CSR_DLBOUND11HI     0x8a7
+#define CSR_DLBOUND12LO     0x8a8
+#define CSR_DLBOUND12HI     0x8a9
+#define CSR_DLBOUND13LO     0x8aa
+#define CSR_DLBOUND13HI     0x8ab
+#define CSR_DLBOUND14LO     0x8ac
+#define CSR_DLBOUND14HI     0x8ad
+#define CSR_DLBOUND15LO     0x8ae
+#define CSR_DLBOUND15HI     0x8af
 
 #define CSR_DMAINCALL       0x8b0
 #define CSR_DRETURNPC       0x8b1
@@ -71,12 +66,12 @@
 
 #define CSR_DJBOUND0LO      0x8c0
 #define CSR_DJBOUND0HI      0x8c1
-// #define CSR_DJBOUND1LO      0x8c2
-// #define CSR_DJBOUND1HI      0x8c3
-// #define CSR_DJBOUND2LO      0x8c4
-// #define CSR_DJBOUND2HI      0x8c5
-// #define CSR_DJBOUND3LO      0x8c6
-// #define CSR_DJBOUND3HI      0x8c7
+#define CSR_DJBOUND1LO      0x8c2
+#define CSR_DJBOUND1HI      0x8c3
+#define CSR_DJBOUND2LO      0x8c4
+#define CSR_DJBOUND2HI      0x8c5
+#define CSR_DJBOUND3LO      0x8c6
+#define CSR_DJBOUND3HI      0x8c7
 #define CSR_DJCFG           0x8c8
 
 /* DASICS Lib cfg */
